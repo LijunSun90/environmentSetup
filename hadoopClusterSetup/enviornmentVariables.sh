bffcbfc1b528dd185d087b9a5cd7e9aea94afeb8
LijunSun90 <sunlijun522@126.com>
2017-06-24 17:37:27 +0800

    environment variables

---
+++ hadoopClusterSetup/enviornmentVariables.sh
@@ -0,0 +1,8 @@
+#
+##
+### The contents of the enviornmentVariables.sh ### 
+sudo sh -c "echo 'export HADOOP_HOME=/home/myname/softwares/hadoop' >> /etc/profile"
+sudo sh -c "echo 'export PATH=${HADOOP_HOME}/bin:${PATH}' >> /etc/profle"
+### The contents of the enviornmentVariables.sh ### 
+##
+#
